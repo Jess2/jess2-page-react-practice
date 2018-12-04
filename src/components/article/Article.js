2e2410d994f52b1dec3b94056df1780f98b9e5a6
Soyeon Jung <jess2.jsy@gmail.com>
2018-12-04 14:19:17 +0900

    목록에서 게시글을 누르면 해당 게시글의 세부 내용 출력

--- src/components/article/Article.js
+++ src/components/article/Article.js
@@ -3,17 +3,51 @@ import './Article.scss';
 
 export default class Article extends Component {
   state = {
-    contents: "안녕하세요! 테스트 중입니다."
+    contents: "안녕하세요! 테스트 중입니다.",
+    articles: [
+      {
+        id: 1,
+        title: "크로스 도메인(Cross Domain)",
+        date: "2018-12-01",
+        contents: "안녕하세요! 크로스 도메인에 대해서 알아봅시다.",
+      },
+      {
+        id: 2,
+        title: "null과 undefined 비교",
+        date: "2018-12-01",
+        contents: "안녕하세요! null과 undefined에 대해서 알아봅시다.",
+      },
+      {
+        id: 3,
+        title: "이벤트 버블링과 이벤트 캡쳐링",
+        date: "2018-12-02",
+        contents: "안녕하세요! 이벤트 버블링과 캡쳐링에 대해서 알아봅시다."
+      },
+      {
+        id: 4,
+        title: "호이스팅(Hoisting)",
+        date: "2018-12-02",
+        contents: "안녕하세요! 호이스팅에 대해서 알아봅시다.",
+      },
+      {
+        id: 5,
+        title: "onclick과 addEventListener 비교",
+        date: "2018-12-03",
+        contents: "안녕하세요! onclick과 addEventListener에 대해서 알아봅시다.",
+      },
+    ]
   }
   render() {
+    const article = this.state.articles.filter(article => article.id === parseInt(this.props.onArticleId))
+    console.log(article)
     return (
       <div className="article">
         <h3 onClick={this.props.onModeChange}><i className="fa fa-arrow-circle-left"></i> 목록으로 돌아가기</h3>
-        <h1>onclick과 addEventListener 비교</h1>
-        <p>2018-12-03</p>
+        <h1>{article[0].title}</h1>
+        <p>{article[0].date}</p>
         <textarea
           readOnly
-          value={this.state.contents}
+          value={article[0].contents}
         />
       </div>
     );
--- src/components/es5/JavaScript.js
+++ src/components/es5/JavaScript.js
@@ -5,26 +5,60 @@ import Article from 'components/article/Article';
 export default class JavaScript extends Component {
   state = {
     readMode: false,
+    articleId: 0,
+    articles: [
+      {
+        id: 1,
+        title: "크로스 도메인(Cross Domain)",
+        date: "2018-12-01",
+      },
+      {
+        id: 2,
+        title: "null과 undefined 비교",
+        date: "2018-12-01",
+      },
+      {
+        id: 3,
+        title: "이벤트 버블링과 이벤트 캡쳐링",
+        date: "2018-12-02",
+      },
+      {
+        id: 4,
+        title: "호이스팅(Hoisting)",
+        date: "2018-12-02",
+      },
+      {
+        id: 5,
+        title: "onclick과 addEventListener 비교",
+        date: "2018-12-03",
+      },
+    ]
   }
-  modeChange = () => {
+  modeChange = (e) => {
+    this.state.articleId = e.target.id;
     this.setState({
       readMode: !this.state.readMode
     })
   }
   render() {
+    const articles = this.state.articles.reverse().map(
+      ({id, title, date}, i) => (
+        <li id={id} onClick={this.modeChange} key={i}>
+          <span className="liNumber">{id}</span>
+          {title}
+          <span className="liDate">{date}</span>
+        </li>
+      )
+    )
     return (
       <div>
         {
           this.state.readMode === false ?
           <div className="JavaScript">
           <h1>JavaScript</h1>
-          <p>게시물 수: 5</p>
+          <p>게시물 수: {articles.length}</p>
           <ul>
-            <li onClick={this.modeChange}><span className="liNumber">5</span>onclick과 addEventListener 비교<span className="liDate">2018-12-03</span></li>
-            <li onClick={this.modeChange}><span className="liNumber">4</span>호이스팅(Hoisting)<span className="liDate">2018-12-03</span></li>
-            <li onClick={this.modeChange}><span className="liNumber">3</span>이벤트 버블링과 이벤트 캡쳐링<span className="liDate">2018-12-03</span></li>
-            <li onClick={this.modeChange}><span className="liNumber">2</span>null과 undefined 비교<span className="liDate">2018-12-03</span></li>
-            <li onClick={this.modeChange}><span className="liNumber">1</span>크로스 도메인(Cross Domain)<span className="liDate">2018-12-03</span></li>
+            {articles}
           </ul>
   
           <div className="paginationWrapper">
@@ -44,10 +78,10 @@ export default class JavaScript extends Component {
         </div>
         :
         <Article
+          onArticleId={this.state.articleId}
           onModeChange={this.modeChange}
         />
         }
-        
       </div>
     );
   }
